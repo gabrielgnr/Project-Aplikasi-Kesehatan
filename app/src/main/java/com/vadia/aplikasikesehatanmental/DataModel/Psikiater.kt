f92713ee48b93e98d59d2e643e3d79e39566cb9c
gabrielgnr <56532692+gabrielgnr@users.noreply.github.com>
2020-11-19 22:15:48 +0700

    Data Model, tambahan minor

--- app/src/main/java/com/vadia/aplikasikesehatanmental/DataModel/Psikiater.kt
+++ app/src/main/java/com/vadia/aplikasikesehatanmental/DataModel/Psikiater.kt
@@ -1,2 +1,9 @@
 package com.vadia.aplikasikesehatanmental.DataModel
 
+data class Psikiater(
+    val id: String = "",
+    val fullName: String = "",
+    val email: String = "",
+    val phone: String = "",
+    val dateOfBirth:String=""
+)

e76a4f5fef8b04ac0766b7c0f231bba7a72b8825
rafelsiregar <rafel.siregar@gmail.com>
2020-11-19 21:09:23 +0700

    Memperbaiki halaman login

--- app/src/main/java/com/vadia/aplikasikesehatanmental/LoginActivity.kt
+++ app/src/main/java/com/vadia/aplikasikesehatanmental/LoginActivity.kt
@@ -2,11 +2,23 @@ package com.vadia.aplikasikesehatanmental
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private val firebaseAuth : FirebaseAuth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_page)
 
+
+        initUI()
+    }
+
+    private fun initUI(){
+            group_3.setOnClickListener(){
+
+            }
     }
 }

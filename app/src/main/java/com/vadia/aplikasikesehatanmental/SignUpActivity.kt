5b779453eaf0c3223add41352e7e51cf0d7af9bb
gabrielgnr <56532692+gabrielgnr@users.noreply.github.com>
2020-11-19 22:35:27 +0700

    tambahan minor

--- app/src/main/java/com/vadia/aplikasikesehatanmental/SignUpActivity.kt
+++ app/src/main/java/com/vadia/aplikasikesehatanmental/SignUpActivity.kt
@@ -7,9 +7,13 @@ import android.os.Bundle
 import android.util.Patterns
 import android.widget.EditText
 import android.widget.RadioButton
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.vadia.aplikasikesehatanmental.DataModel.Pasien
 
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -17,11 +21,12 @@ class SignUpActivity : AppCompatActivity() {
     private val db = Firebase.firestore
     private val pasien= Firebase.firestore.collection("pasien")
     private val psikiater= db.collection("psikiater")
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
+        auth = Firebase.auth
         initUI()
     }
 
@@ -56,6 +61,34 @@ class SignUpActivity : AppCompatActivity() {
 
     }
 
+
+    }
+
+    private fun addPasien(fullname:String,username:String,email:String,noTelp:String,password:String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+    private fun updateUI(nothing: Nothing?) {
+
+    }
+
+    private fun addPsikiater(){
 
     }
 }

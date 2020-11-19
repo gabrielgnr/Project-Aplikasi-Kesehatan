d772b71d33b218d96c118e6e00df81254616e3f8
gabrielgnr <56532692+gabrielgnr@users.noreply.github.com>
2020-11-19 17:21:58 +0700

    Tambahan Plugin

--- app/src/main/java/com/vadia/aplikasikesehatanmental/SignUpActivity.kt
+++ app/src/main/java/com/vadia/aplikasikesehatanmental/SignUpActivity.kt
@@ -1,11 +1,34 @@
 package com.vadia.aplikasikesehatanmental
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.EditText
 
 class SignUpActivity : AppCompatActivity() {
+    //private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    //private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        initUI()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        /*
+        if (firebaseAuth.currentUser != null)
+            startActivity(Intent(this, HomeActivity::class.java))
+
+         */
+    }
+
+    private fun initUI() {
+    val fullName=findViewById<EditText>(R.id.fullname)
+
+
     }
 }

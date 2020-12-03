511bed05b9f2119ad98179cd11c74a189c44287c
rafelsiregar <rafel.siregar@gmail.com>
2020-12-03 23:02:52 +0700

    Mengcomment bagian yang error

--- app/src/main/java/com/vadia/aplikasikesehatanmental/MenuActivity.kt
+++ app/src/main/java/com/vadia/aplikasikesehatanmental/MenuActivity.kt
@@ -33,7 +33,7 @@ class MenuActivity : AppCompatActivity() {
         }*/
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
+        //val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
@@ -41,8 +41,8 @@ class MenuActivity : AppCompatActivity() {
                 R.id.nav_edit, R.id.nav_setting, R.id.nav_logout
             ), drawerLayout
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        /*setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)*/
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -52,8 +52,8 @@ class MenuActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+        /*val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()*/
     }
 
 }

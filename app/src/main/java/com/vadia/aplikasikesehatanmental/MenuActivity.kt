d561dff223b3d7e1eec8e2a28c41869a4d815de6
rafelsiregar <rafel.siregar@gmail.com>
2020-12-04 11:40:47 +0700

    Melakukan tambahan bagian pada rencana pembuatan Navbar

--- app/src/main/java/com/vadia/aplikasikesehatanmental/MenuActivity.kt
+++ app/src/main/java/com/vadia/aplikasikesehatanmental/MenuActivity.kt
@@ -1,22 +1,19 @@
 package com.vadia.aplikasikesehatanmental
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 
-class MenuActivity : AppCompatActivity() {
+class MenuActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -34,26 +31,78 @@ class MenuActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         //val navController = findNavController(R.id.nav_host_fragment)
+        val toggle : ActionBarDrawerToggle = ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close)
+        if (drawerLayout != null) {
+            drawerLayout.addDrawerListener(toggle)
+        }
+        toggle.syncState()
+
+        if(navView!=null){
+            navView.setNavigationItemSelectedListener(this)
+        }
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_edit, R.id.nav_setting, R.id.nav_logout
-            ), drawerLayout
+                setOf(
+                        R.id.nav_edit, R.id.nav_setting, R.id.nav_logout
+                ), drawerLayout
         )
         /*setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)*/
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    override fun onBackPressed() {
+        val drawer : DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawer != null) {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START)
+            } else {
+                super.onBackPressed()
+            }
+        }
+    }
+
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu, menu)
         return true
+    }*/
+
+    /*override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }*/
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
+        return when (item.itemId) {
+            R.id.nav_edit -> {
+                // Handle the camera import action (for now display a toast).
+                val intent = Intent(this, EditProfileActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            R.id.nav_setting -> {
+                // Handle the gallery action (for now display a toast).
+                true
+            }
+            R.id.nav_logout -> {
+                // Handle the slideshow action (for now display a toast).
+                drawer.closeDrawer(GravityCompat.START)
+                FirebaseAuth.getInstance().signOut()
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> false
+        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        /*val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()*/
-    }
 
 }
